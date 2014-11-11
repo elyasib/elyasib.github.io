113064c29fee7cbeca5773844d80ce96527eaf9b
elyasib <elyasib.isc@gmail.com>
2014-11-11 08:12:42 -0600

    Scaling corrected

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -19,6 +19,35 @@ var isMobile = {
   }, 
   any: function() { 
     return (isMobile.Android() || isMobile.BlackBerry() || isMobile.IPhone() || isMobile.Windows() || isMobile.OperaMini() || isMobile.FFoxMobile()); 
-  }
+  } 
 }; 
 
+
+var contentSize = {
+  isNarrow: function(){
+    var thisElement = document.querySelector("#docWrap"); 
+    var aspect = document.documentElement.clientWidth/document.documentElement.clientHeight; 
+
+    if (aspect <= 0.7 && !isMobile.any()) { 
+      if(thisElement.classList.contains("outerFull")){thisElement.classList.remove("outerFull");thisElement.classList.add("outerSmall");} 
+      wSize.setAttribute("value","narrow"); 
+    } else { 
+      if(thisElement.classList.contains("outerSmall")){thisElement.classList.remove("outerSmall");thisElement.classList.add("outerFull");} 
+      wSize.setAttribute("value","wide"); 
+    };
+  },
+
+  initWrapper: function(){ 
+    //alert("hola");
+    var thisElement = document.querySelector("#docWrap"); 
+    var aspect = document.documentElement.clientWidth/document.documentElement.clientHeight;
+    if (isMobile.any() || aspect <= 0.7){ 
+      thisElement.classList.add("outerSmall"); 
+    } else { 
+      //alert("hola");
+      thisElement.classList.add("outerFull"); 
+    }; 
+  }
+};
+
+window.onresize = contentSize.isNarrow; 
