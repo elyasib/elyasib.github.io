7a132059fba9e5e52ff2b1820628c16843f70008
elyasib <elyasib.isc@gmail.com>
2014-11-12 07:36:17 -0600

    Scaling of iform fixed

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -19,35 +19,52 @@ var isMobile = {
   }, 
   any: function() { 
     return (isMobile.Android() || isMobile.BlackBerry() || isMobile.IPhone() || isMobile.Windows() || isMobile.OperaMini() || isMobile.FFoxMobile()); 
-  } 
+  }, 
 }; 
 
+var mobFlg = isMobile.any(); 
 
-var contentSize = {
-  isNarrow: function(){
+var contentSize = { 
+  isNarrow: function(){ 
+  if (!mobFlg) { 
     var thisElement = document.querySelector("#docWrap"); 
     var aspect = document.documentElement.clientWidth/document.documentElement.clientHeight; 
 
-    if (aspect <= 0.7 && !isMobile.any()) { 
+    if (aspect <= 0.7) { 
       if(thisElement.classList.contains("outerFull")){thisElement.classList.remove("outerFull");thisElement.classList.add("outerSmall");} 
-      wSize.setAttribute("value","narrow"); 
     } else { 
       if(thisElement.classList.contains("outerSmall")){thisElement.classList.remove("outerSmall");thisElement.classList.add("outerFull");} 
-      wSize.setAttribute("value","wide"); 
-    };
-  },
+      }; 
+    }; 
+  }, 
 
   initWrapper: function(){ 
-    //alert("hola");
     var thisElement = document.querySelector("#docWrap"); 
-    var aspect = document.documentElement.clientWidth/document.documentElement.clientHeight;
-    if (isMobile.any() || aspect <= 0.7){ 
+    var aspect = document.documentElement.clientWidth/document.documentElement.clientHeight; 
+    if (mobFlg || aspect <= 0.7){ 
       thisElement.classList.add("outerSmall"); 
     } else { 
-      //alert("hola");
       thisElement.classList.add("outerFull"); 
     }; 
-  }
+  } 
+}; 
+
+function rltiveScaling(base,toScale){
+  var b = document.getElementById(toScale); 
+  a = document.getElementById(base);
+  if (a.offsetWidth*.9 < 350) {
+    z = (a.offsetWidth*.9)/350; 
+    b.width = b.offsetWidth*(a.offsetWidth/b.offsetWidth);
+    b.height = b.offsetHeight*(a.offsetWidth/b.offsetWidth);
+    s = "-ms-zoom: "+z+"; -moz-transform: scale("+z+"); -moz-transform-origin: 0 0; -o-transform: scale("+z+"); -o-transform-origin: 0 0; -webkit-transform: scale("+z+"); -webkit-transform-origin: 0 0;"; 
+    if (typeof b.setAttribute === "function") b.setAttribute('style', s); 
+    else if (typeof b.style.setAttribute === "object") b.style.setAttribute('cssText', s); 
+  }; 
+};
+
+function scaleIframe(){
+  return rltiveScaling("mainContent","contactIframe");
 };
 
 window.onresize = contentSize.isNarrow; 
+
